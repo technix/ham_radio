c70315c038712cfc6a1afef4af5d2024233b9525
techniX <sergei.mozhaisky@gmail.com>
2019-12-07 21:54:21 +0200

    better validation

--- helpers.lua
+++ helpers.lua
@@ -1,10 +1,9 @@
 function ham_radio.validate_frequency(frequency)
-  if frequency == nil then
-    return false
-  end
-  local num_freq = math.floor(tonumber(frequency))
+  local num_freq = tonumber(frequency)
   local freq = tostring(num_freq)
   return freq == frequency
+    and num_freq ~= nil
+    and num_freq == math.floor(num_freq)
     and num_freq >= ham_radio.settings.frequency.min
     and num_freq <= ham_radio.settings.frequency.max
 end
